16f2f939f7d982988ea154d9da7e0867d7aec518
Camille <camille.aussems@selexis.com>
2022-08-26 11:06:43 +0200

    Moved the TMC4361A to an external folder

---
+++ M4Decapv1/M4Decapv1.ino
@@ -0,0 +1,19 @@
+#include <SPI.h>
+#include "TMC4361A_Register.h"
+#include "Arduino.h"
+#include "PwmOut.h"
+#include "pinDefinitions.h"
+#include "PinNames.h"
+#define _TIMERINTERRUPT_LOGLEVEL_ 0
+#include "Portenta_H7_TimerInterrupt.h"
+#include "TMC4361A.h"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
--- M7Decapv1/TMC2660_REG.h
+++
@@ -1,9 +0,0 @@
-#ifndef TMC2660_REG_H
-#define TMC2660_REG_H
-	uint32_t DRVCTRL_REG;
-	uint32_t CHOPCONF_REG;
-	uint32_t SMARTEN_REG;
-	uint32_t SGSCONF_REG;
-	uint32_t DRVCONF_REG;
-
-#endif//TMC2660_REG_H
